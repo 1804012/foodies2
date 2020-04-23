0d3b9bf1fc132a91bc845dd4cdeeae46a67a8d9f
Félix Richer <1804012@cstj.qc.ca>
2020-04-23 13:41:57 -0400

    finition du breakpoint tab-horizontal

--- 00_DivisionDesModules/02_js/script.js
+++ 00_DivisionDesModules/02_js/script.js
@@ -5,17 +5,18 @@
 
  $(function(){
     /* script pour l'ouverture et la fermeture de la navigation mobile*/
-    $('i.menu').on('click', function(){
-        $('nav').removeClass('fermer').addClass('ouvert');
-        $('body').addClass('unscrollable');
-        $("html, body").animate({scrollTop: 0}, 0);
-    });
-    $('i.close').on('click', function(){
-        $('nav').removeClass('ouvert').addClass('fermer');
-        $('body').removeClass('unscrollable');
-    });
-    
+    if(window.outerWidth >= 1920) {
+        document.getElementById('navigation').className = 'ouvert';
+    } else {
+        $('i.menu').on('click', function(){
+            $('nav').removeClass('fermer').addClass('ouvert');
+            $('body').addClass('unscrollable');
+            $("html, body").animate({scrollTop: 0}, 0);
+        });
+        $('i.close').on('click', function(){
+            $('nav').removeClass('ouvert').addClass('fermer');
+            $('body').removeClass('unscrollable');
+        });
+    }
     /* fin du script pour l'ouverture et la fermeture de la navigation mobile */
-
-
 });
